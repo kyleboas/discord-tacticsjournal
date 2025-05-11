ec3f55d7eeb98ef46c4d17701f43d85629a9e8dc
Kyle Boas <68482183+kyleboas@users.noreply.github.com>
2025-05-10 21:37:02 -0400

    added teams

---
+++ teams.js
@@ -0,0 +1,14 @@
+// teams.js
+export const VALID_TEAMS = [
+  'Arsenal',
+  'Man City',
+  'Liverpool',
+  'Chelsea',
+  'Barcelona',
+  'Real Madrid',
+  'Valencia',
+  'Benfica',
+  'Como',
+  'Anderlecht'
+  // Add more as needed
+];
