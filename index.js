88b46db2a12d76ec7313c04e8a32d3e40b5658b6
Kyle Boas <68482183+kyleboas@users.noreply.github.com>
2025-05-12 18:35:45 -0400

    .

--- index.js
+++ index.js
@@ -84,9 +84,9 @@ client.on('interactionCreate', async interaction => {
           const { position, team } = ref;
 
           await msg.edit({
-            content: `Added to watchlist by <@${userId}>\n**${avg}** | ${position} | ${name} (${team})`,
-            components: components
-          });
+          content: `Added to watchlist by <@${ref.userId}>\n**${avg}** | ${position} | ${name} (${team})`,
+          components
+        });
         } catch (err) {
           console.error('Failed to edit message for score update:', err);
         }
