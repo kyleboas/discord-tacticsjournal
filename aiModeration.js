b2f03ed168f9453db0b725ab12a37881f2084088
Kyle Boas <68482183+kyleboas@users.noreply.github.com>
2025-05-17 12:21:58 -0400

    .

--- aiModeration.js
+++ aiModeration.js
@@ -84,7 +84,7 @@ export const TRIGGER_PATTERNS = {
   ],
   
   DIRECT_TARGETING: [
-   /\bf+[\s._-]*[uuv]+[\s._-]*[c(kq)]+[\s._-]*k+[\s._-]*(@?[a-zA-Z0-9_]{2,})\b/i
+    /\bf+[\s._-]*[uuv]+[\s._-]*[c(kq)]+[\s._-]*k+\s+(@?[a-zA-Z][a-zA-Z0-9_]{2,})\b/i
   ]
 };
 
